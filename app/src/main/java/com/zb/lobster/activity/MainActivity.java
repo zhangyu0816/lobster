e1750af310d160c7607e6b5008e269198b8d864c
章榆 <wx_93b92f7c19d347d690fd6efd259103e0@git.code.tencent.com>
2020-04-01 10:38:18 +0800

    添加公共包

--- app/src/main/java/com/zb/lobster/activity/MainActivity.java
+++ app/src/main/java/com/zb/lobster/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.zb.lobster;
+package com.zb.lobster.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.zb.lobster.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // sss
-
     }
 }
