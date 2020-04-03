b4b609c974a67837c239550404e60d9b22d9b163
e <wx_93b92f7c19d347d690fd6efd259103e0@git.code.tencent.com>
2020-04-03 09:38:30 +0800

    上传图片

--- module_register/src/main/java/com/zb/module_register/LogoActivity.java
+++ module_register/src/main/java/com/zb/module_register/LogoActivity.java
@@ -1,14 +1,19 @@
 package com.zb.module_register;
 
+import android.view.ViewGroup;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.zb.lib_base.activity.BaseActivity;
+import com.zb.lib_base.app.MineApp;
 import com.zb.lib_base.utils.RouteUtils;
+import com.zb.module_register.databinding.RegisterLogoBinding;
 import com.zb.module_register.vm.LogoViewModel;
 
 @Route(path = RouteUtils.Register_Logo)
 public class LogoActivity extends BaseActivity {
 
     private LogoViewModel viewModel;
+    private RegisterLogoBinding binding;
 
     @Override
     public int getRes() {
@@ -20,5 +25,16 @@ public class LogoActivity extends BaseActivity {
         viewModel = new LogoViewModel();
         viewModel.setBinding(mBinding);
         mBinding.setVariable(BR.viewModel, viewModel);
+
+        binding = (RegisterLogoBinding) mBinding;
+
+        ViewGroup.LayoutParams lp = binding.includeLayout.whiteView.getLayoutParams();
+        lp.width = MineApp.W * 5 / 6;
+        binding.includeLayout.whiteView.setLayoutParams(lp);
+
+        ViewGroup.LayoutParams lpur = binding.uploadRelative.getLayoutParams();
+        lpur.height=  lpur.width = (int) (MineApp.W * 600f/ 1080f);
+        binding.uploadRelative.setLayoutParams(lpur);
+
     }
 }
