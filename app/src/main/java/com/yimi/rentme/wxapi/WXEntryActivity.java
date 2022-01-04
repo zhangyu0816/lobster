6c120f2f9c35db3001f63100541abe36d9cb0aca
zhangyu <9194964+zhangyu0816@user.noreply.gitee.com>
2022-01-04 16:38:33 +0800

    7.13 1

--- app/src/main/java/com/yimi/rentme/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/yimi/rentme/wxapi/WXEntryActivity.java
@@ -1,12 +1,16 @@
 package com.yimi.rentme.wxapi;
 
-
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
 import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 public class WXEntryActivity extends WXCallbackActivity {
     @Override
     public void onResp(BaseResp resp) {
-        finish();
+        if (resp.getType() == ConstantsAPI.COMMAND_LAUNCH_WX_MINIPROGRAM) {
+            finish();
+        } else {
+            super.onResp(resp);//一定要加super，实现我们的方法，否则不能回调   重重重要   关键在于这一句
+        }
     }
 }
