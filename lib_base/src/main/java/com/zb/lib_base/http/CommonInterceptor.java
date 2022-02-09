cece423968b98a8d40eeef9f201f8ea303e48281
zhangyu <9194964+zhangyu0816@user.noreply.gitee.com>
2022-02-09 14:12:32 +0800

    7.13 3

--- lib_base/src/main/java/com/zb/lib_base/http/CommonInterceptor.java
+++ lib_base/src/main/java/com/zb/lib_base/http/CommonInterceptor.java
@@ -1,8 +1,5 @@
 package com.zb.lib_base.http;
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -15,12 +12,9 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -106,8 +100,6 @@ public class CommonInterceptor implements Interceptor {
                 }
                 params = buffer.readString(charset);
             }
-        }else if("GET".equalsIgnoreCase(method)){
-
         }
 
         treeMap.put("pfDevice", "Android");
@@ -138,73 +130,4 @@ public class CommonInterceptor implements Interceptor {
         return chain.proceed(newRequest);
     }
 
-    /**
-     * 获取应用签名
-     *
-     * @param context
-     * @param packageName
-     * @return
-     */
-    public static String getSign(Context context, String packageName) {
-        try {
-            PackageInfo info = context.getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
-            byte[] bytes = info.signatures[0].toByteArray();
-            return byte2hex(bytes).toLowerCase(Locale.ROOT);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
-    /**
-     * 获取签名的MD5值
-     *
-     * @param context
-     * @param packageName
-     * @return
-     */
-    public static String getSignMD5(Context context, String packageName) {
-        try {
-            PackageInfo info = context.getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
-            byte[] bytes = info.signatures[0].toByteArray();
-            return encryptionMD5(bytes);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
-    private static String encryptionMD5(byte[] byteStr) {
-        MessageDigest messageDigest = null;
-        StringBuffer md5StrBuff = new StringBuffer();
-        try {
-            messageDigest = MessageDigest.getInstance("MD5");
-            messageDigest.reset();
-            messageDigest.update(byteStr);
-            byte[] byteArray = messageDigest.digest();
-            for (int i = 0; i < byteArray.length; i++) {
-                if (Integer.toHexString(0xFF & byteArray[i]).length() == 1) {
-                    md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
-                } else {
-                    md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
-                }
-            }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return md5StrBuff.toString();
-    }
-
-    private static String byte2hex(byte[] bytes) {
-        String hs = "";
-        String tmp = "";
-        for (int i = 0; i < bytes.length; i++) {
-            tmp = (Integer.toHexString(bytes[i] & 0XFF));
-            if (tmp.length() == 1)
-                hs = hs + "0" + tmp;
-            else
-                hs = hs + tmp;
-        }
-        return hs.toUpperCase();
-    }
 }
